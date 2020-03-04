70a64157470252cbce4f18a157f50d077d14348c
jonah <jonah.n.rubin@gmail.com>
2020-03-04 12:24:54 -0500

    listing weights and biases

--- Network.py
+++ Network.py
@@ -13,13 +13,17 @@ class Net:
         self.lr = lr
         self.epochs = epochs
 
+        # store sizes and instances of weights and biases
+        self.w_sizes = [(x.shape[1],ls), (self.ls, self.ls), (self.ls, y.shape[1])]
+        self.b_sizes = [(1,self.ls), (1, self.ls), (1, y.shape[1])]
+        self.ws = []
+        self.bs = []
+
         # weights and biases for layers
-        self.w1 = np.random.randn(x.shape[1], self.ls)
-        self.b1 = np.zeros((1, ls))
-        self.w2 = np.random.randn(self.ls, self.ls)
-        self.b2 = np.zeros((1, self.ls))
-        self.w3 = np.random.randn(self.ls,  y.shape[1])
-        self.b3 = np.zeros((1,  y.shape[1]))
+        for w, b in zip(self.w_sizes, self.b_sizes):
+            self.ws.append(np.random.randn(w[0], w[1]))
+            self.bs.append(np.zeros((b[0], b[1])))
+
 
     # training loop
     def train(self):
@@ -30,11 +34,11 @@ class Net:
                 print('Error for epoch ' + str(x) + ': ' + str(self.loss))
 
     def feed(self):
-        z1 = np.dot(self.x, self.w1) + self.b1   # feed layer 1
+        z1 = np.dot(self.x, self.ws[0]) + self.bs[0]   # feed layer 1
         self.a1 = sig(z1)
-        z2 = np.dot(self.a1, self.w2) + self.b2  # feed layer 2
+        z2 = np.dot(self.a1, self.ws[1]) + self.bs[1]  # feed layer 2
         self.a2 = sig(z2)
-        z3 = np.dot(self.a2, self.w3) + self.b3  # feed output
+        z3 = np.dot(self.a2, self.ws[2]) + self.bs[2]  # feed output
 
         # get normalized max of output layer
         self.a3 = softmax(z3)
@@ -43,18 +47,18 @@ class Net:
         self.loss = error(self.a3, self.y)
 
         a3_delta = cross(self.a3, self.y)        # adjustments for layer 3->2
-        z2_delta = np.dot(a3_delta, self.w3.T)
+        z2_delta = np.dot(a3_delta, self.ws[2].T)
         a2_delta = z2_delta * dsig(self.a2)      # adjustments for layer 2->1
-        z1_delta = np.dot(a2_delta, self.w2.T)
+        z1_delta = np.dot(a2_delta, self.ws[1].T)
         a1_delta = z1_delta * dsig(self.a1)      # adjustments for layer 1
 
         # tuning for layers
-        self.w3 -= self.lr * np.dot(self.a2.T, a3_delta)
-        self.b3 -= self.lr * np.sum(a3_delta, axis=0, keepdims=True)
-        self.w2 -= self.lr * np.dot(self.a1.T, a2_delta)
-        self.b2 -= self.lr * np.sum(a2_delta, axis=0)
-        self.w1 -= self.lr * np.dot(self.x.T, a1_delta)
-        self.b1 -= self.lr * np.sum(a1_delta, axis=0)
+        self.ws[2] -= self.lr * np.dot(self.a2.T, a3_delta)
+        self.bs[2] -= self.lr * np.sum(a3_delta, axis=0, keepdims=True)
+        self.ws[1] -= self.lr * np.dot(self.a1.T, a2_delta)
+        self.bs[1] -= self.lr * np.sum(a2_delta, axis=0)
+        self.ws[0] -= self.lr * np.dot(self.x.T, a1_delta)
+        self.bs[0] -= self.lr * np.sum(a1_delta, axis=0)
 
     def predict(self, data):
         self.x = data
